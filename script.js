f1c0e30341ca0d40be8e7f400b7b55eac6979895
BSquad <121628964+BSquad@users.noreply.github.com>
2025-11-29 13:06:34 +0100

    refactoring

--- script.js
+++ script.js
@@ -1,112 +1,49 @@
-async function fetchProducts() {
+async function fetchTSV(url) {
   //https://docs.google.com/spreadsheets/d/1dTOeVckrXhczMe1M2IH5WsL817teLYTqHtMI0hLQDto/edit?gid=0#gid=0
-  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vREBTSfAxtL3CUUCYdfq18N96hbNra9mSQP7NjkolG--a0DeveIkb0QZhtsm39yqDCAjtebofyHod42/pub?gid=0&output=tsv";
-  const response = await fetch(url);
-  const tsvText = await response.text();
-  const products = parseProductsTSV(tsvText);
-
-  return products;
-}
-
-async function fetchCategories() {
-  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vREBTSfAxtL3CUUCYdfq18N96hbNra9mSQP7NjkolG--a0DeveIkb0QZhtsm39yqDCAjtebofyHod42/pub?gid=771625926&output=tsv";
-  const response = await fetch(url);
-  const tsvText = await response.text();
-  const categories = parseCategoriesTSV(tsvText);
-
-  return categories;
-}
-
-function parseProductsTSV(tsvText) {
+  const res = await fetch(url);
+  const tsvText = await res.text();
   const lines = tsvText.trim().split("\n");
   const headers = lines.shift().split("\t").map(h => h.trim());
-
-  return lines.map(line => {
-    const values = line.split("\t").map(v => v.trim());
-    return {
-      category: values[headers.indexOf("Kategorie")],
-      name: values[headers.indexOf("Name")],
-      description: values[headers.indexOf("Beschreibung")],
-      price: values[headers.indexOf("Preis")],
-      imageName: values[headers.indexOf("Bild")]
-    };
-  });
+  return lines.map(line => Object.fromEntries(headers.map((h, i) => [h, line.split("\t")[i]?.trim()])));
 }
 
-function parseCategoriesTSV(tsvText) {
-  const lines = tsvText.trim().split("\n");
-  const headers = lines.shift().split("\t").map(h => h.trim());
-
-  const map = {};
-
-  for (const line of lines) {
-    const values = line.split("\t").map(v => v.trim());
-    const name = values[headers.indexOf("Name")];
-    const description = values[headers.indexOf("Beschreibung")];
-
-    if (name) {
-      map[name] = description;
-    }
-  }
-
-  return map;
-}
-
-function groupByCategory(products) {
-  const categories = {};
-  for (const p of products) {
-    if (!categories[p.category]) categories[p.category] = [];
-    categories[p.category].push(p);
-  }
-  return categories;
-}
-
-function createCategoryInfoElement(category, categoryInfo) {
-  if (!categoryInfo[category]) return null;
-
-  const div = document.createElement("div");
-  div.className = "category-info";
-  div.textContent = categoryInfo[category];
-  div.innerHTML = categoryInfo[category].replace(/\\n/g, "<br>");
-  return div;
-}
-
-function createProductElement(product) {
-  const div = document.createElement("div");
-  const imageUrl = product.imageName
-    ? "images/" + product.imageName
-    : null;
-  div.className = "product";
-  div.innerHTML = `
-    <div class="product-content">
-      <div class="product-text">
-        <div class="name">${product.name}</div>
-        <div class="desc">${product.description}: ${product.price}</div>
-      </div>
-      ${imageUrl
-      ? `<img class="product-img" src="${imageUrl}" alt="${product.name}">`
-      : ""
-    }
-    </div>
-  `;
-  return div;
+function assignProductsToCategories(categoriesRaw, productsRaw) {
+  const categoryInfo = Object.fromEntries(categoriesRaw.map(c => [c.Name, c.Beschreibung]));
+  const categories = productsRaw.reduce((acc, p) => {
+      if (!acc[p.Kategorie]) acc[p.Kategorie] = [];
+      acc[p.Kategorie].push(p);
+      return acc;
+    }, {});
+  return { categories, categoryInfo };
 }
 
 
-function renderCategories(container, categories, categoryInfo) {
+function renderContent(container, categories, categoryInfo) {
   container.innerHTML = "";
 
-  for (const category in categories) {
-    const h2 = document.createElement("h2");
-    h2.textContent = category;
-    container.appendChild(h2);
+  for (const [catName, prods] of Object.entries(categories)) {
+    container.appendChild(Object.assign(document.createElement("h2"), { textContent: catName }));
 
-    const infoElem = createCategoryInfoElement(category, categoryInfo);
-    if (infoElem) container.appendChild(infoElem);
+    if (categoryInfo[catName]) {
+      const infoDiv = document.createElement("div");
+      infoDiv.className = "category-info";
+      infoDiv.innerHTML = categoryInfo[catName].replace(/\\n/g, "<br>");
+      container.appendChild(infoDiv);
+    }
 
-    for (const product of categories[category]) {
-      const prodElem = createProductElement(product);
-      container.appendChild(prodElem);
+    for (const p of prods) {
+      const div = document.createElement("div");
+      div.className = "product";
+      const img = p.Bild ? `<img class="product-img" src="images/${p.Bild}" alt="${p.Name}">` : "";
+      div.innerHTML = `
+          <div class="product-content">
+            <div class="product-text">
+              <div class="name">${p.Name}</div>
+              <div class="desc">${p.Beschreibung}: ${p.Preis}</div>
+            </div>
+            ${img}
+          </div>`;
+      container.appendChild(div);
     }
   }
 }
@@ -117,18 +54,17 @@ async function createContent() {
   loader.classList.remove("hidden");
 
   try {
-    const [products, categoryInfo] = await Promise.all([
-      fetchProducts(),
-      fetchCategories()
+    const [productsRaw, categoriesRaw] = await Promise.all([
+      fetchTSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vREBTSfAxtL3CUUCYdfq18N96hbNra9mSQP7NjkolG--a0DeveIkb0QZhtsm39yqDCAjtebofyHod42/pub?gid=0&output=tsv"),
+      fetchTSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vREBTSfAxtL3CUUCYdfq18N96hbNra9mSQP7NjkolG--a0DeveIkb0QZhtsm39yqDCAjtebofyHod42/pub?gid=771625926&output=tsv")
     ]);
-
-    const categories = groupByCategory(products);
-    loader.classList.add("hidden");
-    renderCategories(container, categories, categoryInfo);
+    const { categories, categoryInfo } = assignProductsToCategories(categoriesRaw, productsRaw);
+    renderContent(container, categories, categoryInfo);
   } catch (err) {
-    loader.classList.add("hidden");
     container.innerHTML = "<p>Fehler beim Laden der Produkte</p>";
     console.error(err);
+  } finally {
+    loader.classList.add("hidden");
   }
 }
 
